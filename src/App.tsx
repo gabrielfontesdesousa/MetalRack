6102304ecf0c7e1e3bedff6b57677e6f6efe34c4
gabrielfontesdesousa <gabriel.sousa235@etec.sp.gov.br>
2025-05-20 22:54:06 -0300

    corrigindo caminho de imagens

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import SpecificationsArea from './components/specifications'
 import ExampleArea from './components/example'
 import FormArea from './components/formarea'
 import Footer from './components/footer'
+import background from './assets/bg.png'
 function App() {
 
   return (
-    <main className='@apply bg-[linear-gradient(_to_bottom,transparent_25%,black_95%),url("./src/assets/bg.png")] h-screen max-w-[px] bg-cover bg-center z-[-1];'>
+    <main className='h-screen max-w-[px] bg-cover bg-center z-[-1];' style={{ backgroundImage: `linear-gradient(to bottom, transparent 25%, black 95%), url(${background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    width: '100%',}}>
       <Header></Header>
         <Home></Home>
         <SpecificationsArea></SpecificationsArea>
